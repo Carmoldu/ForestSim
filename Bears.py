45ec19f4e86b83351a866ddab88cb0e6a6ff79fc
MOLINS DURAN Carles <carles.molinsduran@altran.com>
2020-05-16 20:27:46 +0200

    Added basic structure to Bears and Lumberjacks to test initialization of Forest

---
+++ Bears.py
@@ -0,0 +1,6 @@
+class Bear:
+    def __init__(self):
+        pass
+
+    def display(self):
+        return "B"
---
+++ Lumberjacks.py
@@ -0,0 +1,6 @@
+class Lumberjack:
+    def __init__(self):
+        pass
+
+    def display(self):
+        return "L"
